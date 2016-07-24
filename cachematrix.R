b4c8ce41fbd39af682bc5b97f23c67efed37be78
Seamus Mc Gonigle <seamus.mcgonigle@workday.com>
2016-07-24 13:40:19 +0100

    Add Cache Matrix Implementations

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are for the caching of an inverse matrix
+## The cached matrix itself is stored in the 'matrix' object, 
+## while the logic to check if the cached value is present is in the 
+## cacheSolve function.
 
-## Write a short comment describing this function
-
+## Create an object which contains the vector, possibly the cached inverse and getters/setters for each. 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+    set <- function(y) {
+           x <<- y
+           m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) m <<- inverse
+    getInverse <- function() m
+    list(set = set, get = get,
+    setInverse = setInverse,
+    getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Get the cached inverse or compute it if not present.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   m <- x$getInverse()
+   if(!is.null(m)) {
+       message("getting cached data")
+       return(m)
+   }
+   data <- x$get()
+   m <- solve(data, ...)
+   x$setInverse(m)
+   m
 }
